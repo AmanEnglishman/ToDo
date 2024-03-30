fc062df1ee39477d04adc5ddd4f46648f3fdfcb5
Aman <aman13032006@gmail.com>
2024-03-30 17:28:29 +0600

    update urls

--- task/urls.py
+++ task/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('task_list/', TaskListView.as_view(), name='task-list'),
     path('task_create/', TaskCreateView.as_view(), name='task-create'),
     path('task_delete/', TaskDeleteView.as_view(), name='task-delete'),
-    path('task_update/', TaskUpdateView.as_view(), name='task-update'),
-    path('task_detail/<int:id>', TaskDetailView.as_view(), name='task-detail'),
+    path('task_update/<int:pk>', TaskUpdateView.as_view(), name='task-update'),
+    path('task_detail/<int:pk>', TaskDetailView.as_view(), name='task-detail'),
 ]
